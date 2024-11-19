a8c3a472e29ef30043cbf81f607704995b5d8c27
shipperizer <alexcabb@gmail.com>
2024-11-19 20:02:48 +0000

    fixup! 3a09baa

--- modules/k8s/cloudinit.tf
+++ modules/k8s/cloudinit.tf
@@ -1,3 +1,25 @@
+
+data "template_file" "proxy" {
+  template = file("${path.module}/scripts/http-proxy.conf")
+
+  vars = {
+    https_proxy  = var.https_proxy
+    http_proxy   = var.http_proxy
+    no_proxy     = data.openstack_networking_subnet_v2.subnet.cidr
+    service_cidr = "10.152.183.0/24"
+    pod_cidr     = "10.1.0.0/16"
+  }
+}
+
+
+data "template_file" "nodes" {
+  template = file("${path.module}/scripts/nodes.yaml")
+
+  vars = {
+    http_proxy_file = data.template_file.proxy.rendered
+  }
+}
+
 data "template_cloudinit_config" "k8s" {
   gzip          = false
   base64_encode = false
@@ -7,7 +29,7 @@ data "template_cloudinit_config" "k8s" {
     merge_type = "list(append)+dict(recurse_array)+str()"
 
     content_type = "text/cloud-config"
-    content      = file("${path.module}/scripts/nodes.yaml")
+    content      = data.template_file.nodes.rendered
   }
 }
 
@@ -21,9 +43,9 @@ data "template_cloudinit_config" "k8s_leader" {
     merge_type = "list(append)+dict(recurse_array)+str()"
 
     content_type = "text/cloud-config"
-    content      = file("${path.module}/scripts/nodes.yaml")
+    content      = data.template_file.nodes.rendered
   }
-
+  
   part {
     filename   = "leader.cfg"
     merge_type = "list(append)+dict(recurse_array)+str()"
